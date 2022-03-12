d1caa7d27d8d56389bb7287de433b141e8b38f5c
Janes Thomas <janesthomas1995@googlemail.com>
2022-03-12 14:28:41 +0100

    project init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.ngwn.f1udp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.ngwn.f1udp to javafx.fxml;
+    exports com.ngwn.f1udp;
+}
